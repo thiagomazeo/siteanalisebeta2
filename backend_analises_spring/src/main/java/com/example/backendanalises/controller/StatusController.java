04bc8c210ba0964f6d8fb5b63cee6720dc07e186
thiagomazeo <thiagomazeo@gmail.com>
2025-06-02 10:28:13 -0300

    faz o simples

---
+++ backend_analises_spring/src/main/java/com/example/backendanalises/controller/StatusController.java
@@ -0,0 +1,14 @@
+package com.example.backendanalises.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StatusController {
+
+    @GetMapping("/api/status")
+    public String status() {
+        return "Backend funcionando";
+    }
+
+}
