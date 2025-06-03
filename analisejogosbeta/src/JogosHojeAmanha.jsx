1aae73d8095e40c18a541fbe25edb2a9ee9c8cbe
thiagomazeo <thiagomazeo@gmail.com>
2025-06-03 11:23:20 -0300

    first commit

---
+++ analisejogosbeta/src/JogosHojeAmanha.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const JogosHojeAmanha = () => {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">Jogos de Hoje e Amanhã</h1>
+      <p>Em breve os dados aparecerão aqui...</p>
+    </div>
+  )
+}
+
+export default JogosHojeAmanha
